49a8516db9779838516a0a54447197c809331c97
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-10 23:30:05 +0300

    Answer Page added

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,6 +3,7 @@ import Layout from "./pages/Layout/Layout";
 import Landing from "./pages/Landing/Landing";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Question from "./pages/Question/Question";
 
 function AppRouter() {
   return (
@@ -10,6 +11,7 @@ function AppRouter() {
       <Route path="/" element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Landing />} />
+        <Route path="/question" element={<Question />} />
       </Route>
     </Routes>
   );
