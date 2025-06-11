4f3d69b3a06cf1bf9ae9a11ed2c44f7b8327ca0b
kasimnurlgn <kasimnurlgn9@gmail.com>
2025-06-11 10:22:50 +0300

    How to style further modified

--- src/pages/HowItWorks/HowItWorks.jsx
+++ src/pages/HowItWorks/HowItWorks.jsx
@@ -14,7 +14,7 @@ const HowItWorks = () => {
           To join Evangadi Networks Q&A, you need to create an account. Follow
           these steps:
         </p>
-        <ol>
+        <ul>
           <li>Click on the "Sign In" button in the top-right corner.</li>
           <li>Switch to the "Create a new account" form.</li>
           <li>
@@ -26,23 +26,23 @@ const HowItWorks = () => {
             You will receive a confirmation message upon successful
             registration.
           </li>
-        </ol>
+        </ul>
       </section>
 
       <section className={classes.howto_section}>
         <h2>User Login</h2>
         <p>Once you have registered, you can log in to your account:</p>
-        <ol>
+        <ul>
           <li>Click on the "Sign In" button in the top-right corner.</li>
           <li>Enter your registered Email and Password.</li>
           <li>Click on the "Login" button to access your account.</li>
-        </ol>
+        </ul>
       </section>
 
       <section className={classes.howto_section}>
         <h2>Asking a Question</h2>
         <p>To ask a new question:</p>
-        <ol>
+        <ul>
           <li>
             After logging in, click on the "Ask Question" button on the Home
             page.
@@ -56,13 +56,13 @@ const HowItWorks = () => {
             Your question will appear on the Home page for the community to view
             and answer.
           </li>
-        </ol>
+        </ul>
       </section>
 
       <section className={classes.howto_section}>
         <h2>Viewing Questions and Answers</h2>
         <p>To browse and view questions and their answers:</p>
-        <ol>
+        <ul>
           <li>Navigate to the Home page to see a list of recent questions.</li>
           <li>
             Click on a question title to view its details and existing answers.
@@ -71,32 +71,32 @@ const HowItWorks = () => {
             If there are no answers, you'll see a prompt encouraging you to
             answer.
           </li>
-        </ol>
+        </ul>
       </section>
 
       <section className={classes.howto_section}>
         <h2>Submitting an Answer</h2>
         <p>To answer a question:</p>
-        <ol>
+        <ul>
           <li>Navigate to the question you want to answer.</li>
           <li>Scroll down to the "Answer The Top Question" section.</li>
           <li>Type your answer in the provided textarea.</li>
           <li>Click on the "Post Your Answer" button to submit.</li>
           <li>Your answer will appear under the community answers section.</li>
-        </ol>
+        </ul>
       </section>
 
       <section className={classes.howto_section}>
         <h2>Logging Out</h2>
         <p>To securely log out of your account:</p>
-        <ol>
+        <ul>
           <li>
             Click on the "Logout" button located in the header/navigation bar.
           </li>
           <li>
             This will clear your session and redirect you to the login page.
           </li>
-        </ol>
+        </ul>
       </section>
 
       <section className={classes.howto_section}>
